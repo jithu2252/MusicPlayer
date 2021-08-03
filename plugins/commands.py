5c564a06379182171e9600db1434451c74350687
jithu2252 <87238581+jithu2252@users.noreply.github.com>
2021-08-03 22:05:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -69,12 +69,12 @@ You can also use /dplay <song name> to play a song from Deezer.</b>
 async def start(client, message):
     buttons = [
         [
-        InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-        InlineKeyboardButton('🤖 Other Bots', url='https://t.me/subin_works/122'),
+        InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/trooporiginals'),
+        InlineKeyboardButton('🤖 Other Bots', url='https://t.me/trooporiginals/72'),
     ],
     [
-        InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/subinps'),
-        InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+        InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/troopbuckybot'),
+        InlineKeyboardButton('🧩 Source', url='https://t.me/troopbuckybot'),
     ],
     [
         InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help'),
@@ -92,12 +92,12 @@ async def start(client, message):
 async def show_help(client, message):
     buttons = [
         [
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('🤖 Other Bots', url='https://t.me/subin_works/122'),
+            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/trooporiginals'),
+            InlineKeyboardButton('🤖 Other Bots', url='https://t.me/trooporiginals/72'),
         ],
         [
-            InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/subinps'),
-            InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+            InlineKeyboardButton('👨🏼‍💻 Developer', url='t.me/troopbuckybot'),
+            InlineKeyboardButton('🧩 Source', url='t.me/troopbuckybot'),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
